f2ccd642a5d3fb8ea15744a327a7eb0d4cda2f24
mancha-heinz <manchaheinz@gmail.com>
2023-07-14 18:22:05 -0300

    commit_2

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,35 @@
+import Botao from '../Botao/indesx';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+    const times = [
+        'programação',
+        'front-end',
+        'data science',
+        'devops',
+        'ux e design',
+        'mobile',
+        'inovação e gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('form foi enviado')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados p/ criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="informe uma imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+                <Botao>
+                    criar card
+                </Botao>
             </form>
         </section>
     )
